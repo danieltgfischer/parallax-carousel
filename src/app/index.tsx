d5dc8020b42214a1a0a415477820dfa46bb3a119
Daniel Fischer <danieltgfischer@gmail.com>
2025-03-16 22:59:29 -0300

    feat: integrate Wallpapers component into Home screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import { Wallpapers } from "@/components";
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold">Hello World</Text>
+    <View
+      className="flex-1 items-center justify-center bg-white"
+      style={{ flex: 1, flexGrow: 1 }}
+    >
+      <Wallpapers />
     </View>
   );
 }
